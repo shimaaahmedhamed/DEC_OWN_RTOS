d36c4f1e0ea907c159c20bd2136f142b5aa615b4
shimaaahmedhamed <120210302+shimaaahmedhamed@users.noreply.github.com>
2023-01-18 11:32:27 +0200

    Add files via upload

---
+++ Debug/HAL/LCD/LCD_program.d
@@ -0,0 +1,26 @@
+HAL/LCD/LCD_program.o HAL/LCD/LCD_program.o: ../HAL/LCD/LCD_program.c \
+  ../HAL/LCD/LCD_configure.h ../HAL/LCD/LCD_interface.h \
+  ../HAL/LCD/LCD_private.h ../HAL/LCD/../../LIB/LBIT_MATHH.h \
+  ../HAL/LCD/../../LIB/LSTD_TYPES.h \
+  ../HAL/LCD/../../MCAL/DIO/DIO_interface.h \
+  ../HAL/LCD/../../MCAL/DIO/DIO_private.h \
+  ../HAL/LCD/../../MCAL/DIO/../../LIB/LBIT_MATH.h \
+  ../HAL/LCD/../../MCAL/DIO/../../LIB/LSTD_TYPES.h
+
+../HAL/LCD/LCD_configure.h:
+
+../HAL/LCD/LCD_interface.h:
+
+../HAL/LCD/LCD_private.h:
+
+../HAL/LCD/../../LIB/LBIT_MATHH.h:
+
+../HAL/LCD/../../LIB/LSTD_TYPES.h:
+
+../HAL/LCD/../../MCAL/DIO/DIO_interface.h:
+
+../HAL/LCD/../../MCAL/DIO/DIO_private.h:
+
+../HAL/LCD/../../MCAL/DIO/../../LIB/LBIT_MATH.h:
+
+../HAL/LCD/../../MCAL/DIO/../../LIB/LSTD_TYPES.h:
---
+++ Debug/HAL/SS/subdir.mk
@@ -0,0 +1,24 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../HAL/SS/SSD_Programe.c 
+
+OBJS += \
+./HAL/SS/SSD_Programe.o 
+
+C_DEPS += \
+./HAL/SS/SSD_Programe.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+HAL/SS/%.o: ../HAL/SS/%.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: AVR Compiler'
+	avr-gcc -Wall -g2 -gstabs -O0 -fpack-struct -fshort-enums -ffunction-sections -fdata-sections -std=gnu99 -funsigned-char -funsigned-bitfields -mmcu=atmega32 -DF_CPU=8000000UL -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -c -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
